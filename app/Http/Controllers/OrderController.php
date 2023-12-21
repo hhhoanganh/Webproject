2d8e4eb0e1b7518e89af7c0abcfb672acb46790a
hhhoanganh <101188848+hhhoanganh@users.noreply.github.com>
2023-12-21 11:02:49 +0700

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,7 +15,7 @@ class OrderController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     public function addOrders(Request $request)
