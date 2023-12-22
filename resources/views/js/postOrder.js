5560eea07a1989b2d111907deb5e321c1cd4d3d8
serdap <92677014+serdap@users.noreply.github.com>
2023-12-22 12:21:25 +0700

    call api Order

---
+++ resources/views/js/postOrder.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Gọi API ở đây
+
+    function fetchData(page) {
+        // Hàm tải dữ liệu từ API dựa trên trang hiện tại
+        url = `http://127.0.0.1:8000/api/v1/orders?page=${page}`
+        const options = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data => {
+                const totalPages = data.meta.last_page;
+                displayData(data.data);
+                displayPagination(totalPages);
+            })
+            .catch(error => {
+                console.error('Lỗi khi tải dữ liệu:', error);
+            });
+    }
+    fetchData(currentPage);
+});
